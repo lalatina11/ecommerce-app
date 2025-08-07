c2f671a83c15aef12f5df64163e35429aff733da
lalatina11 <rahmadancandra@gmail.com>
2025-08-07 22:17:07 +0700

    style: swap style for moon and sun

--- admin/src/components/ModeToggle.tsx
+++ admin/src/components/ModeToggle.tsx
@@ -24,8 +24,8 @@ export function ModeToggle(props: Props) {
         return (
             <Button size={"icon"} variant={isGhost ? "ghost" : "default"}
                     onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>
-                <Moon
+                <Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>
+                <Sun
                     className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"/>
             </Button>
         )
@@ -34,9 +34,9 @@ export function ModeToggle(props: Props) {
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <Sun
-                        className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>
                     <Moon
+                        className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90"/>
+                    <Sun
                         className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0"/>
                     <span className="sr-only">Toggle theme</span>
                 </Button>
