1352baed3d644e6192bbb378d38f95422ab0d235
lalatina11 <rahmadancandra@gmail.com>
2025-08-11 19:57:37 +0700

    feat(chore): add bar chart and area chart compt

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,5 +1,6 @@
-import {ModeToggle} from "@/components/ModeToggle";
 import {Metadata} from "next";
+import AppBarChart from "@/components/AppBarChart";
+import AppAreaChart from "@/components/AppAreaChart";
 
 export const metadata: Metadata = {
     title: "iShop Admin | Home",
@@ -9,11 +10,15 @@ export const metadata: Metadata = {
 const Page = () => {
     return (
         <div className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4"}>
-            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>test</div>
+            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>
+                <AppBarChart/>
+            </div>
             <div className={"bg-primary-foreground rounded-lg p-4"}>test</div>
             <div className={"bg-primary-foreground rounded-lg p-4"}>test</div>
             <div className={"bg-primary-foreground rounded-lg p-4"}>test</div>
-            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>test</div>
+            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>
+                <AppAreaChart/>
+            </div>
             <div className={"bg-primary-foreground rounded-lg p-4"}>test</div>
         </div>
     )
---
+++ admin/src/components/AppAreaChart.tsx
@@ -0,0 +1,101 @@
+"use client";
+import {
+    type ChartConfig,
+    ChartContainer,
+    ChartLegend,
+    ChartLegendContent,
+    ChartTooltip,
+    ChartTooltipContent
+} from "@/components/ui/chart"
+import {Area, AreaChart, CartesianGrid, XAxis, YAxis} from "recharts";
+
+const chartData = [
+    {month: "January", desktop: 186, mobile: 80},
+    {month: "February", desktop: 305, mobile: 200},
+    {month: "March", desktop: 237, mobile: 120},
+    {month: "April", desktop: 73, mobile: 190},
+    {month: "May", desktop: 209, mobile: 130},
+    {month: "June", desktop: 214, mobile: 140},
+]
+
+
+const chartConfig = {
+    desktop: {
+        label: "Desktop",
+        color: "var(--chart-4)",
+    },
+    mobile: {
+        label: "Mobile",
+        color: "var(--chart-1)",
+    },
+} satisfies ChartConfig
+
+const AppAreaChart = () => {
+    return (
+        <div>
+            <span className={"text-lg font-medium mb-6"}>Total Revenue</span>
+            <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+                <AreaChart accessibilityLayer data={chartData}>
+                    <CartesianGrid vertical={false}/>
+                    <XAxis
+                        dataKey="month"
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={true}
+                        tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <YAxis
+                        tickLine={false}
+                        tickMargin={10}
+                        axisLine={true}
+                        // tickFormatter={(value) => value.slice(0, 3)}
+                    />
+                    <defs>
+                        <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                            <stop
+                                offset="5%"
+                                stopColor="var(--color-desktop)"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="var(--color-desktop)"
+                                stopOpacity={0.1}
+                            />
+                        </linearGradient>
+                        <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                            <stop
+                                offset="5%"
+                                stopColor="var(--color-mobile)"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="var(--color-mobile)"
+                                stopOpacity={0.1}
+                            />
+                        </linearGradient>
+                    </defs>
+                    <ChartTooltip content={<ChartTooltipContent/>}/>
+                    <ChartLegend content={<ChartLegendContent/>}/>
+                    <Area
+                        dataKey="desktop"
+                        type="natural"
+                        fill="url(#fillDesktop)"
+                        fillOpacity={0.4}
+                        stroke="var(--chart-4)"
+                        stackId="a"/>
+                    <Area
+                        dataKey="mobile"
+                        type="natural"
+                        fill="url(#fillMobile)"
+                        fillOpacity={0.4}
+                        stroke="var(--chart-1)"
+                        stackId="b"/>
+                </AreaChart>
+            </ChartContainer>
+        </div>
+    )
+}
+
+export default AppAreaChart
