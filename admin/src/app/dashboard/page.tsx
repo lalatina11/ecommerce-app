089a281703ccc24cf63885cfb768bc335c57312a
lalatina11 <rahmadancandra@gmail.com>
2025-09-11 18:28:35 +0700

    feat: apply logo for all pages

--- admin/src/app/dashboard/page.tsx
+++ admin/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import {Metadata} from "next";
 export const metadata: Metadata = {
     title: 'iShop Admin | Dashboard',
     description: "iShop Admin page",
-    icons:"/logo.svg",
+    icons:"logo.svg",
 }
 const Page = () => {
     return (
--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Metadata} from "next";
+import { Metadata } from "next";
 import AppBarChart from "@/components/AppBarChart";
 import AppAreaChart from "@/components/AppAreaChart";
 import AppPieChart from "@/components/AppPieChart";
@@ -6,33 +6,44 @@ import CardList from "@/components/CardList";
 import TodoList from "@/components/TodoList";
 
 export const metadata: Metadata = {
-    title: "iShop Admin | Home",
-    description: "iShop Admin Dashboard",
-}
+  title: "iShop Admin | Home",
+  description: "iShop Admin Dashboard",
+  icons: "logo.svg",
+};
 
 const Page = () => {
-    return (
-        <div className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 pb-4"}>
-            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>
-                <AppBarChart/>
-            </div>
-            <div className={"bg-primary-foreground rounded-lg p-4"}>
-                <CardList title={"Latest Transaction"}/>
-            </div>
-            <div className={"bg-primary-foreground rounded-lg p-4"}>
-                <AppPieChart/>
-            </div>
-            <div className={"bg-primary-foreground rounded-lg p-4"}>
-                <TodoList/>
-            </div>
-            <div className={"bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"}>
-                <AppAreaChart/>
-            </div>
-            <div className={"bg-primary-foreground rounded-lg p-4"}>
-                <CardList title={"Popular Content"}/>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      className={"grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4 pb-4"}
+    >
+      <div
+        className={
+          "bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"
+        }
+      >
+        <AppBarChart />
+      </div>
+      <div className={"bg-primary-foreground rounded-lg p-4"}>
+        <CardList title={"Latest Transaction"} />
+      </div>
+      <div className={"bg-primary-foreground rounded-lg p-4"}>
+        <AppPieChart />
+      </div>
+      <div className={"bg-primary-foreground rounded-lg p-4"}>
+        <TodoList />
+      </div>
+      <div
+        className={
+          "bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2"
+        }
+      >
+        <AppAreaChart />
+      </div>
+      <div className={"bg-primary-foreground rounded-lg p-4"}>
+        <CardList title={"Popular Content"} />
+      </div>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
