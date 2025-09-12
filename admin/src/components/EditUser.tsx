c70e8693f6e3d905203f29f4b711c244b5cf4e83
lalatina11 <rahmadancandra@gmail.com>
2025-09-12 12:47:22 +0700

    feat: add shadcn react hook form to edit user info

--- admin/src/components/EditUser.tsx
+++ admin/src/components/EditUser.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -8,15 +9,29 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
-import { Edit } from "lucide-react";
-import { FormEventHandler, useState } from "react";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { toast } from "sonner";
-import { z, email, object, string } from "zod";
-import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Edit } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { email, object, string, z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Props {
   userInformation: {
@@ -48,11 +63,11 @@ const EditUser = ({ userInformation }: Props) => {
     defaultValues: userInformation,
   });
 
-  const handleEditUserProfile: FormEventHandler<HTMLFormElement> = async (
-    e
+  const handleEditUserProfile = async (
+    values: z.infer<typeof updateProfileUserSchema>,
   ) => {
-    e.preventDefault();
     toast.success("Berhasil edit profile user!");
+    console.log(values);
     setIsOpen(false);
   };
 
@@ -67,21 +82,87 @@ const EditUser = ({ userInformation }: Props) => {
         <SheetHeader>
           <SheetTitle className="mb-4">Edit User Information</SheetTitle>
           <SheetDescription asChild>
-            <form onSubmit={handleEditUserProfile} className="space-y-6">
-              <fieldset className="flex flex-col gap-3">
-                <Label className="capitalize" htmlFor="username">
-                  username
-                </Label>
-                <Input name="username" placeholder="username" />
-              </fieldset>
-              <fieldset className="flex flex-col gap-3">
-                <Label className="capitalize" htmlFor="email">
-                  email
-                </Label>
-                <Input name="email" placeholder="email" />
-              </fieldset>
-              <Button className="w-full">Process</Button>
-            </form>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleEditUserProfile)}
+                className="space-y-6"
+              >
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="capitalize">username</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="capitalize">email</FormLabel>
+                      <FormControl>
+                        <Input type="email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="capitalize">phone</FormLabel>
+                      <FormControl>
+                        <Input type="tel" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="location"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="capitalize">location</FormLabel>
+                      <FormControl>
+                        <Input type="tel" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="role"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="capitalize">role</FormLabel>
+                      <FormControl>
+                        <Select onValueChange={field.onChange}>
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Role" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="user">User</SelectItem>
+                            <SelectItem value="admin">Admin</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button className="w-full">Process</Button>
+              </form>
+            </Form>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
