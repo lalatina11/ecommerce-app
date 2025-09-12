98bb254e86008fd8a233c0e488ac11a1a0455068
lalatina11 <rahmadancandra@gmail.com>
2025-09-12 18:33:33 +0700

    style: sticky navbar

--- admin/src/components/Navbar.tsx
+++ admin/src/components/Navbar.tsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
-import {ModeToggle} from "@/components/ModeToggle";
+import { ModeToggle } from "@/components/ModeToggle";
 import UserDropdownMenu from "@/components/UserDropdownMenu";
-import {SidebarTrigger} from "@/components/ui/sidebar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const Navbar = () => {
-    return (
-        <header className={"flex p-4 items-center justify-between"}>
-            {/*LEFT*/}
-            <SidebarTrigger />
-            {/*Right*/}
-            <nav className={"flex items-center justify-between gap-4"}>
-                <Link href={"/dashboard"}>Dashboard</Link>
-                <ModeToggle/>
-                <UserDropdownMenu/>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header
+      className={
+        "flex p-4 items-center justify-between sticky top-0 bg-background z-10 border-b border-zinc-500 mb-3"
+      }
+    >
+      {/*LEFT*/}
+      <SidebarTrigger />
+      {/*Right*/}
+      <nav className={"flex items-center justify-between gap-4"}>
+        <Link href={"/dashboard"}>Dashboard</Link>
+        <ModeToggle />
+        <UserDropdownMenu />
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
