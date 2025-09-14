71363309e00e0b8590a2f4c98d9f97873beaacca
lalatina11 <rahmadancandra@gmail.com>
2025-09-14 16:17:59 +0700

    chore: apply payments compt ui

--- admin/src/app/payments/page.tsx
+++ admin/src/app/payments/page.tsx
@@ -1,5 +1,17 @@
-const Page = () => {
-  return <div>Hei this is payment page</div>;
+import { columns } from "@/components/payments/column";
+import { DataTable } from "@/components/payments/DataTable";
+import paymentsData from "@/lib/payments";
+
+const Page = async () => {
+  const data = await paymentsData();
+  return (
+    <div className="p-3 pb-6">
+      <div className="mb-8 px-4 py-2 bg-secondary rounded-md">
+        <h1 className="font-semibold">All Payments</h1>
+      </div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 };
 
 export default Page;
