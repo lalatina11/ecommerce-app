925a5b58072e5762606438a8f7e92706e0a9be8f
lalatina11 <rahmadancandra@gmail.com>
2025-09-15 11:18:26 +0700

    chore: homepage desiging
    home page designing, add ads image and product list section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,28 @@
-import {ModeToggle} from "@/components/ModeToggle";
-import {Metadata} from "next";
+import ProductList from "@/components/ProductList";
+import { Metadata } from "next";
+import Image from "next/image";
 
-export const metadata:Metadata={
-    title:"iShop | Home",
-    description:"iShop is an home page",
-}
+export const metadata: Metadata = {
+  title: "iShop | Home",
+  description: "iShop is an home page",
+  icons: "/logo.svg",
+};
 
 const Page = () => {
   return (
-      <div>
-        <span>Hello world</span>
-      <ModeToggle isDropdown/>
+    <div className="flex flex-col w-full min-h-screen">
+      <div className="bg-background p-6 rounded-lg shadow shadow-zinc-500 mt-6 ring ring-zinc-500 mb-5">
+        <Image
+          src={"/featured.png"}
+          alt="featuredProduct"
+          width={2000}
+          height={2000}
+          className="object-cover w-full h-auto"
+        />
       </div>
-  )
-}
+      <ProductList />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
