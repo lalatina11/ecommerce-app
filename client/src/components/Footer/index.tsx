decc8f7336577ac88b53f6b980203b0cb91e0fef
lalatina11 <rahmadancandra@gmail.com>
2025-09-15 08:27:26 +0700

    style: coloring footer
    change footer background and text color into shadcn color

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg">
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-background p-8 rounded-lg">
       <div className="flex flex-col gap-4 items-center md:items-start">
         <Link href="/" className="flex items-center">
           <Image src="/logo.svg" alt="iShop" width={36} height={36} />
@@ -11,25 +11,25 @@ const Footer = () => {
             iShop
           </p>
         </Link>
-        <p className="text-sm text-gray-400">© 2025 iShop</p>
-        <p className="text-sm text-gray-400">All rights reserved.</p>
+        <p className="text-sm text-muted-foreground">© 2025 iShop</p>
+        <p className="text-sm text-muted-foreground">All rights reserved.</p>
       </div>
-      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50">Pages</p>
+      <div className="flex flex-col gap-4 text-sm text-muted-foreground items-center md:items-start">
+        <p className="text-sm text-primary font-semibold">Pages</p>
         <Link href="/">Homepage</Link>
         <Link href="/">Contact</Link>
         <Link href="/">Terms of Service</Link>
         <Link href="/">Privacy Policy</Link>
       </div>
-      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50">Products</p>
+      <div className="flex flex-col gap-4 text-sm text-muted-foreground items-center md:items-start">
+        <p className="text-sm text-primary font-semibold">Products</p>
         <Link href="/">All Products</Link>
         <Link href="/">New Arrivals</Link>
         <Link href="/">Best Sellers</Link>
         <Link href="/">Sale</Link>
       </div>
-      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
-        <p className="text-sm text-amber-50">Extra</p>
+      <div className="flex flex-col gap-4 text-sm text-muted-foreground items-center md:items-start">
+        <p className="text-sm text-primary font-semibold">Extra</p>
         <Link href="/">About</Link>
         <Link href="/">Contact</Link>
         <Link href="/">Blog</Link>
