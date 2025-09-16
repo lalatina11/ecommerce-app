5e8eb32903cf35a943f2b9674b6b128380fbc389
lalatina11 <rahmadancandra@gmail.com>
2025-09-16 14:02:56 +0700

    feat: add select product size

--- client/src/components/ProductList/ProductCard.tsx
+++ client/src/components/ProductList/ProductCard.tsx
@@ -1,26 +1,35 @@
 "use client";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { ProductType } from "@/types";
-import { Card, CardFooter } from "../ui/card";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
-import { Label } from "../ui/label";
 import { FaCartPlus } from "react-icons/fa";
 import { buttonVariants } from "../ui/button";
+import { Card, CardFooter } from "../ui/card";
+import { Label } from "../ui/label";
 
 interface Props {
   product: ProductType;
 }
 
 const ProductCard = ({ product }: Props) => {
-  const [productColor, setProductColor] = useState(product.colors[0]);
+  const [selectProductColor, setSelectProductColor] = useState(
+    product.colors[0]
+  );
+  const [selectProductSize, setselectProductSize] = useState(product.sizes[0]);
 
   return (
-    <Card className="p-3 bg-background overflow-hidden pb-5">
+    <Card className="p-4 bg-background overflow-hidden pb-5">
       <Link href={`/products/${product.id}`}>
         <Image
-          src={product.images[productColor]}
+          src={product.images[selectProductColor]}
           alt={product.name}
           width={500}
           height={500}
@@ -28,44 +37,78 @@ const ProductCard = ({ product }: Props) => {
         />
       </Link>
       <CardFooter className="flex flex-col gap-2 w-full px-0">
-        <div className="flex gap-1 flex-col w-full justify-normal">
-          <Label className="text-sm">Color</Label>
-          <div className="flex items-center gap-1">
-            {product.colors.map((color) => (
-              <div
-                style={{
-                  backgroundColor: color,
-                }}
-                key={color}
-                onClick={() => setProductColor(color)}
-                aria-disabled={productColor === color}
-                className="size-4 rounded-full cursor-pointer border border-zinc-500 p-1 relative aria-disabled:cursor-default aria-disabled:opacity-70"
-              >
-                {/* MARKER */}
-                {productColor === color && (
-                  <div
-                    className={`absolute w-3.5 h-1 left-0 top-[5px] rounded-full -rotate-45 ${productColor === color && productColor !== "red" ? "bg-red-500" : productColor === color && color === "red" ? "bg-black" : ""}`}
-                  />
-                )}
-                {/* MARKER END */}
-              </div>
-            ))}
+        <div className="flex justify-between items-center gap-3 w-full">
+          <div className="flex flex-col gap-1 flex-1/2">
+            <Label>Size</Label>
+            <Select
+              onValueChange={(value) => setselectProductSize(value)}
+              value={selectProductSize}
+            >
+              <SelectTrigger className="w-max">
+                <Label className="uppercase">{selectProductSize}</Label>
+              </SelectTrigger>
+              <SelectContent>
+                {product.sizes.map((size) => (
+                  <SelectItem key={size} className="uppercase" value={size}>
+                    {size}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-1 flex-col justify-start h-full flex-1/2">
+            <Label className="text-sm">Color</Label>
+            <div className="flex items-center gap-1">
+              {product.colors.map((color) => (
+                <div
+                  style={{
+                    backgroundColor: color,
+                  }}
+                  key={color}
+                  onClick={() => setSelectProductColor(color)}
+                  aria-disabled={selectProductColor === color}
+                  className="size-4 rounded-full cursor-pointer border border-zinc-500 p-1 relative aria-disabled:cursor-default aria-disabled:opacity-70"
+                >
+                  {/* MARKER */}
+                  {selectProductColor === color && (
+                    <div
+                      className={`absolute w-3.5 h-1 left-0 top-[5px] rounded-full -rotate-45 ${
+                        selectProductColor === color &&
+                        selectProductColor !== "red"
+                          ? "bg-red-500"
+                          : selectProductColor === color && color === "red"
+                          ? "bg-black"
+                          : ""
+                      }`}
+                    />
+                  )}
+                  {/* MARKER END */}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-        <span>{product.name}</span>
+        <Link className="font-semibold" href={`/products/${product.id}`}>
+          {product.name}
+        </Link>
         <span className="text-xs text-muted-foreground">
           {product.shortDescription}
         </span>
-        <Link
-          href={`/products/${product.id}`}
-          className={
-            "w-full flex gap-2 justify-normal items-center" +
-            buttonVariants({ variant: "default" })
-          }
-        >
-          Add to Cart Now!
-          <FaCartPlus />
-        </Link>
+        <div className="flex justify-between items-center w-full mt-2">
+          <Label className="flex-1/2 text-lg font-semibold">
+            ${product.price}
+          </Label>
+          <Link
+            href={`/products/${product.id}`}
+            className={
+              "flex gap-2 justify-normal items-center flex-1/2" +
+              buttonVariants({ variant: "default" })
+            }
+          >
+            Add to Cart
+            <FaCartPlus />
+          </Link>
+        </div>
       </CardFooter>
     </Card>
   );
